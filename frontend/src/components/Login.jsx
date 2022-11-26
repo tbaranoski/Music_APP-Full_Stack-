4b3a7a16b06cde1e7dabc9b9e402f9bc62eff49c
tbaranoski <taylorbaranoski1@gmail.com>
2022-11-26 01:02:02 -0500

    spotify frontpage made

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,8 +3,45 @@ import styled from "styled-components";
 
 //Login function
 export default function Login(){
-    return <Container>login</Container>;
+    
+    //handle click event
+    const handleClick = () =>{
+
+        alert("clicked");
+    };
+
+    return <Container>
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" 
+        alt="spotify">            
+        </img>
+        <button onClick={handleClick}>Connect Spotify</button>
+
+    </Container>;
 
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #1db955;
+    gap: 3rem;
+    img{
+        height: 30vh;
+    }
+    button {
+        width: 30rem;
+        align-items: center;
+        justify-content: center;
+        padding: 1rem 5rem;
+        border-radius: 5rem;
+        border: none;
+        background-color: black;
+        color: #49f585;
+        font-size: 1.6rem;
+        cursor: pointer;
+    }
+`;
